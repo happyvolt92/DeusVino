c24bd0fe924f1a03ad3854ed7e2d6e328d6d2709
egage <egage@netopie.fr>
2023-12-10 10:46:47 +0100

    add sorter button for wine, code in progress for func sort data

--- DeusVino/Controller/SearchViewController.swift
+++ DeusVino/Controller/SearchViewController.swift
@@ -1,62 +1,26 @@
 import UIKit
 
-class SearchViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UISearchBarDelegate {
-    
-    @IBOutlet private weak var wineTypePicker: UIPickerView!
+
+class SearchViewController: UIViewController, UISearchBarDelegate {
+
+    @IBOutlet weak var RedWineSorterButton: UIButton!
+    @IBOutlet weak var WhiteWineSorterButton: UIButton!
     @IBOutlet private weak var searchBar: UISearchBar!
+    @IBOutlet weak var RoseWineSorterButon: UIButton!
+    
     
     private let wineService = WineService()
-    private let wineTypes = ["reds", "whites", "roses"]
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        wineTypePicker.delegate = self
-        wineTypePicker.dataSource = self
         searchBar.delegate = self
     }
-    
-    // MARK: - UIPickerViewDataSource
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return wineTypes.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return wineTypes[row]
-    }
-    
-    // MARK: - UIPickerViewDelegate
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let selectedWineType = wineTypes[row]
-        fetchWines(for: selectedWineType, wineType: selectedWineType.lowercased(), minPrice: nil, maxPrice: nil) { result in
-            // Handle the result here
-            switch result {
-            case .success(let wines):
-                // Access the wines in the array
-                for wine in wines {
-                    let wineType = wine.type
-                    print("Wine Type: \(wineType)")
-                    // Handle other tasks with the wines
-                }
-                
-            case .failure(let error):
-                print("Error: \(error)")
-                // Handle the error
-            }
-        }
-    }
-    
-    
-    // MARK: - UISearchBarDelegate
-    
+
+    // Function called when the search button is clicked
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         if let searchQuery = searchBar.text {
-            // Assuming you want to search for the entered query, modify the fetchWines method accordingly
-            fetchWines(for: searchQuery, wineType: nil, minPrice: nil, maxPrice: nil) { result in
+            // Fetch wines based on the entered search query
+            fetchWines(for: searchQuery) { result in
                 // Handle the result here
                 switch result {
                 case .success(let wines):
@@ -66,7 +30,7 @@ class SearchViewController: UIViewController, UIPickerViewDelegate, UIPickerView
                         print("Wine Type: \(wineType)")
                         // Handle other tasks with the wines
                     }
-                    
+
                 case .failure(let error):
                     print("Error: \(error)")
                     // Handle the error
@@ -75,9 +39,11 @@ class SearchViewController: UIViewController, UIPickerViewDelegate, UIPickerView
         }
         searchBar.resignFirstResponder()
     }
-    
-    private func fetchWines(for wineName: String, wineType: String?, minPrice: Double?, maxPrice: Double?, completion: @escaping (Result<[Wine], Error>) -> Void) {
-        wineService.fetchWines(for: wineName, wineType: wineType, minPrice: minPrice, maxPrice: maxPrice) { result in
+
+    // Function to fetch wines based on the entered search query
+    private func fetchWines(for wineName: String, completion: @escaping (Result<[Wine], Error>) -> Void) {
+        // Call the fetchWines function in WineService
+        wineService.fetchWines(for: wineName) { result in
             completion(result)
         }
     }
--- DeusVino/Utilities/WineService.swift
+++ DeusVino/Utilities/WineService.swift
@@ -1,23 +1,23 @@
 // WineService.swift
+// WineService.swift
 
 import Foundation
 
 class WineService {
-    func fetchWines(for wineName: String, wineType: String?, minPrice: Double?, maxPrice: Double?, completion: @escaping (Result<[Wine], Error>) -> Void) {
+    // Function to fetch wines from the Vivino API based on wine name
+    func fetchWines(for wineName: String, completion: @escaping (Result<[Wine], Error>) -> Void) {
         // Construct the Vivino API URL using the provided parameters
         let apiUrl = "https://api.example.com/vivino"
         
         // Create URL components and add query parameters
         var components = URLComponents(string: apiUrl)!
         components.queryItems = [
-            URLQueryItem(name: "name", value: wineName),
-            URLQueryItem(name: "minPrice", value: minPrice.map { String($0) }),
-            URLQueryItem(name: "maxPrice", value: maxPrice.map { String($0) }),
-            URLQueryItem(name: "wineType", value: wineType)
+            URLQueryItem(name: "name", value: wineName)
         ]
 
         // Validate the URL
         guard let url = components.url else {
+            // If URL is invalid, return an error
             let error = NSError(domain: "AppDomain", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
             completion(.failure(error))
             return
@@ -38,12 +38,18 @@ class WineService {
                 return
             }
 
+            // Print the received data for debugging
+            print("Received Data: \(String(data: data, encoding: .utf8) ?? "Data is not valid UTF-8")")
+
+            // Print the HTTP status code for debugging purposes
+            if let httpResponse = response as? HTTPURLResponse {
+                print("HTTP Status Code: \(httpResponse.statusCode)")
+            }
+
             do {
                 // Parse the JSON data into an array of Wine models
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                let result = try decoder.decode(WineResponse.self, from: data)
-                completion(.success(result.vinos))
+                let wines = try JSONDecoder().decode([Wine].self, from: data)
+                completion(.success(wines))
             } catch {
                 // Handle JSON decoding errors
                 print("Error decoding JSON: \(error)")
